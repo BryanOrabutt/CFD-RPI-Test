9cbe03e8295cee713bf05bf948ea4665362035f0
BryanOrabutt <bryan@bryanorabutt.com>
2019-03-27 21:54:05 -0500

    GUI layout down. Needs logic hooks.

---
+++ src/C/gpio-test/cfd_direct.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "cfd_direct.h"
+
+//Pin accessors
+const unsigned char DATA[8] = {5,6,13,19,26,16,20,21}; //data bus -- used to load configuration registers
+const unsigned char ADDR[4] = {5,6,13,19}; //address bus -- just an alias of the upper nibble of the data bus
+const unsigned char MODE[4] = {26,16,20,21}; //mode bus -- just an alias of the lower nibble of the data bus
+const unsigned char NEG_POL = 7; //negative polarity enable -- POS,NEG = 0,1
+const unsigned char BUS_DIR = 24; //bidirectional bus control -- IN,OUT = 1,0
+const unsigned char STB = 25; //data/address strobe -- addr/mode latched on rising edge, data latched on falling edge
+const unsigned char GEN = 9; //global enable -- OUTPUT,NO_OUTPUT = 1,0
+const unsigned char EXT_AGND = 23; //external AGND enable -- INTERNAL,EXTERNAL = 0,1
+const unsigned char RST_L = 12; //low active reset -- ensures one-shot works properly.
+
+int iter; //iterator for loops
+
+void delay_ns(long ns)
+{
+	struct timespec t, rt;
+	
+	clock_gettime(CLOCK_REALTIME, &rt);
+	rt.tv_nsec = 0;
+	clock_settime(CLOCK_REALTIME, &rt);
+	clock_gettime(CLOCK_REALTIME, &rt);
+	rt.tv_nsec += ns;
+
+	while(1)
+	{
+		clock_gettime(CLOCK_REALTIME, &t);
+		if(rt.tv_nsec > t.tv_nsec)
+			break;
+	}
+}
+
+void setup_io()
+{
+   // open /dev/gpiomem 
+   if ((mem_fd = open("/dev/gpiomem", O_RDWR|O_SYNC) ) < 0) 
+   {
+      printf("can't open /dev/gpiomem \n");
+      exit(-1);
+   }
+
+   //mmap GPIO 
+   gpio_map = mmap(
+      NULL,             //Any adddress in our space will do
+      BLOCK_SIZE,       //Map length
+      PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
+      MAP_SHARED,       //Shared with other processes
+      mem_fd,           //File to map
+      GPIO_BASE         //Offset to GPIO peripheral
+   );
+
+   close(mem_fd); //No need to keep mem_fd open after mmap
+
+   if (gpio_map == MAP_FAILED) 
+   {
+      printf("mmap error %d\n", (int)gpio_map);//errno also set!
+      exit(-1);
+   }
+
+   //Point access pointer at mapped region.
+   gpio = (volatile unsigned *)gpio_map;
+}
+
+//setup GPIO modes and set initial control line states.
+void configure()
+{
+    //Must use INP_GPIO before OUT_GPIO. INP_GPIO will ensure the two MSBs for FSEL are 0
+    //OUT_GPIO will set LSB of FSEL to 1 so that the function select is 001 which is output
+    INP_GPIO(BUS_DIR);
+    OUT_GPIO(BUS_DIR);
+	GPIO_CLR = 1 << BUS_DIR;
+	INP_GPIO(STB);
+    OUT_GPIO(STB);
+	GPIO_CLR = 1 << STB;
+	delay_ns(1000);
+	GPIO_SET = 1 << STB;
+    INP_GPIO(GEN);
+    OUT_GPIO(GEN);
+	GPIO_CLR = 1 << GEN;
+	delay_ns(1000);
+	GPIO_SET = 1 << GEN;
+    INP_GPIO(EXT_AGND);
+    OUT_GPIO(EXT_AGND);
+	GPIO_CLR = 1 << EXT_AGND;
+	delay_ns(1000);
+	GPIO_SET = 1 << EXT_AGND;
+    INP_GPIO(NEG_POL);
+    OUT_GPIO(NEG_POL);
+	GPIO_CLR = 1 << NEG_POL;
+	delay_ns(1000);
+	GPIO_SET = 1 << NEG_POL;
+    INP_GPIO(RST_L);
+    OUT_GPIO(RST_L);
+	GPIO_SET = 1 << RST_L;   
+ 
+    for(iter = 0; iter < 8; iter++)
+    {
+        INP_GPIO(DATA[iter]);
+        OUT_GPIO(DATA[iter]);
+		GPIO_CLR = 1 << DATA[iter];
+    }
+
+    GPIO_SET = 1 << BUS_DIR; //write a 1 to the GPIOSET1 register bit 5
+    GPIO_CLR = 1 << RST_L; //bring RST low to force oneshot state low
+	usleep(1000); //hold RST low for 1 ms
+    GPIO_SET = 1 << RST_L; //bring RST high to enable oneshot
+}
+
+void cleanup_gpio()
+{
+	for(iter = 0; iter < 32; iter++)
+	{
+		GPIO_CLR = 1 << iter;
+		INP_GPIO(iter);
+	}
+}
+
+int main()
+{
+    setup_io();
+    configure();
+
+	for(iter = 0; iter < 8; iter++)
+	{
+		GPIO_SET = 1 << DATA[iter];
+		delay_ns(1000);
+		GPIO_CLR = 1 << DATA[iter];	
+	}
+
+	cleanup_gpio();
+		
+    return(0);
+}
+
---
+++ src/C/gui/cfd_gui.c
@@ -0,0 +1,469 @@
+#include <gtk/gtk.h>
+#include <glib.h>
+
+/* Setup GTK Object handles for each widget */
+
+//global settings check boxes
+GtkWidget *GEN_CB_h;
+GtkWidget *GMode_CB_h;
+GtkWidget *Neg_Pol_CB_h;
+GtkWidget *Internal_AGND_CB_h;
+
+//global settings combo boxes/text boxes
+GtkWidget *Nowlin_Mode_Menu_h;
+GtkWidget *Nowlin_Delay_Menu_h;
+GtkWidget *Test_Point_Menu_h;
+GtkWidget *Lockout_Mode_Menu_h;
+GtkWidget *Lockout_DAC_Box_h;
+GtkWidget *AGND_Trim_Menu_h;
+
+//channel enable checkboxes
+GtkWidget *Channel0_EN_CB_h;
+GtkWidget *Channel1_EN_CB_h;
+GtkWidget *Channel2_EN_CB_h;
+GtkWidget *Channel3_EN_CB_h;
+GtkWidget *Channel4_EN_CB_h;
+GtkWidget *Channel5_EN_CB_h;
+GtkWidget *Channel6_EN_CB_h;
+GtkWidget *Channel7_EN_CB_h;
+GtkWidget *Channel8_EN_CB_h;
+GtkWidget *Channel9_EN_CB_h;
+GtkWidget *Channel10_EN_CB_h;
+GtkWidget *Channel11_EN_CB_h;
+GtkWidget *Channel12_EN_CB_h;
+GtkWidget *Channel13_EN_CB_h;
+GtkWidget *Channel14_EN_CB_h;
+GtkWidget *Channel15_EN_CB_h;
+
+//leading edge DAC text boxes
+GtkWidget *Channel0_LE_DAC_Box_h;
+GtkWidget *Channel1_LE_DAC_Box_h;
+GtkWidget *Channel2_LE_DAC_Box_h;
+GtkWidget *Channel3_LE_DAC_Box_h;
+GtkWidget *Channel4_LE_DAC_Box_h;
+GtkWidget *Channel5_LE_DAC_Box_h;
+GtkWidget *Channel6_LE_DAC_Box_h;
+GtkWidget *Channel7_LE_DAC_Box_h;
+GtkWidget *Channel8_LE_DAC_Box_h;
+GtkWidget *Channel9_LE_DAC_Box_h;
+GtkWidget *Channel10_LE_DAC_Box_h;
+GtkWidget *Channel11_LE_DAC_Box_h;
+GtkWidget *Channel12_LE_DAC_Box_h;
+GtkWidget *Channel13_LE_DAC_Box_h;
+GtkWidget *Channel14_LE_DAC_Box_h;
+GtkWidget *Channel15_LE_DAC_Box_h;
+
+//config button
+GtkWidget *Confiure_Button_h;
+
+/* Callback functions */
+
+void on_GEN_CB_toggled()
+{
+	printf("GEN toggled\n");
+}
+
+void on_GMode_CB_toggled()
+{
+	printf("GMode toggled\n");
+}
+
+void on_Neg_Pol_CB_toggled()
+{
+	printf("Neg pol toggled\n");
+}
+
+void on_Internal_AGND_CB_toggled()
+{
+	printf("Internal AGND toggled\n");
+}
+
+void Nowlin_Mode_Menu_changed_cb()
+{
+	printf("Nowlin mode menu changed\n");
+}
+
+void on_Nowlin_Delay_Menu_changed()
+{
+	printf("Nowlin delay menu changed\n");
+}
+
+void on_Test_Point_Menu_changed()
+{
+	printf("Test point menu changed\n");
+}
+
+void Lockout_Mode_Menu_changed_cb()
+{
+	printf("Lockout mode menu changed\n");
+}
+
+void on_lockout_dac_deleted_text()
+{
+	printf("Lockout DAC textbox: text deleted\n");
+}
+
+void on_lockout_dac_inserted_text()
+{
+	printf("Lockout DAC textbox: text inserted\n");
+}
+
+void on_AGND_Trim_Menu_changed()
+{
+	printf("AGND trim menu changed\n");
+}
+
+void channel_enable_event(int channel)
+{
+	printf("Channel%i enable toggled\n", channel);
+}
+
+void on_Channel0_EN_CB_toggled()
+{
+	channel_enable_event(0);
+}
+
+void on_Channel1_EN_CB_toggled()
+{
+	channel_enable_event(1);
+}
+
+void on_Channel2_EN_CB_toggled()
+{
+	channel_enable_event(2);
+}
+
+void on_Channel3_EN_CB_toggled()
+{
+	channel_enable_event(3);
+}
+
+void on_Channel4_EN_CB_toggled()
+{
+	channel_enable_event(4);
+}
+
+void on_Channel5_EN_CB_toggled()
+{
+	channel_enable_event(5);
+}
+
+void on_Channel6_EN_CB_toggled()
+{
+	channel_enable_event(6);
+}
+
+void on_Channel7_EN_CB_toggled()
+{
+	channel_enable_event(7);
+}
+
+void on_Channel8_EN_CB_toggled()
+{
+	channel_enable_event(8);
+}
+
+void on_Channel9_EN_CB_toggled()
+{
+	channel_enable_event(9);
+}
+
+void on_Channel10_EN_CB_toggled()
+{
+	channel_enable_event(10);
+}
+
+void on_Channel11_EN_CB_toggled()
+{
+	channel_enable_event(11);
+}
+
+void on_Channel12_EN_CB_toggled()
+{
+	channel_enable_event(12);
+}
+
+void on_Channel13_EN_CB_toggled()
+{
+	channel_enable_event(13);
+}
+
+void on_Channel14_EN_CB_toggled()
+{
+	channel_enable_event(14);
+}
+
+void on_Channel15_EN_CB_toggled()
+{
+	channel_enable_event(15);
+}
+
+void channel_text_deleted(int channel)
+{
+	printf("Channel%d DAC box: text deleted\n", channel);
+}
+
+void channel_text_inserted(int channel)
+{
+	printf("Channel%d DAC box: text inserted\n", channel);
+}
+
+void on_ch0_dac_deleted_text()
+{
+	channel_text_deleted(0);
+}
+
+void on_ch0_dac_inserted_text()
+{
+	channel_text_inserted(0);
+}
+
+void on_ch1_dac_deleted_text()
+{
+	channel_text_deleted(1);
+}
+
+void on_ch1_dac_inserted_text()
+{
+	channel_text_inserted(1);
+}
+
+void on_ch2_dac_deleted_text()
+{
+	channel_text_deleted(2);
+}
+
+void on_ch2_dac_inserted_text()
+{
+	channel_text_inserted(2);
+}
+
+void on_ch3_dac_deleted_text()
+{
+	channel_text_deleted(3);
+}
+
+void on_ch3_dac_inserted_text()
+{
+	channel_text_inserted(3);
+}
+
+void on_ch4_dac_deleted_text()
+{
+	channel_text_deleted(4);
+}
+
+void on_ch4_dac_inserted_text()
+{
+	channel_text_inserted(4);
+}
+
+void on_ch5_dac_deleted_text()
+{
+	channel_text_deleted(5);
+}
+
+void on_ch5_dac_inserted_text()
+{
+	channel_text_inserted(5);
+}
+
+void on_ch6_dac_deleted_text()
+{
+	channel_text_deleted(6);
+}
+
+void on_ch6_dac_inserted_text()
+{
+	channel_text_inserted(6);
+}
+
+void on_ch7_dac_deleted_text()
+{
+	channel_text_deleted(7);
+}
+
+void on_ch7_dac_inserted_text()
+{
+	channel_text_inserted(7);
+}
+
+void on_ch8_dac_deleted_text()
+{
+	channel_text_deleted(8);
+}
+
+void on_ch8_dac_inserted_text()
+{
+	channel_text_inserted(8);
+}
+
+void on_ch9_dac_deleted_text()
+{
+	channel_text_deleted(9);
+}
+
+void on_ch9_dac_inserted_text()
+{
+	channel_text_inserted(9);
+}
+
+void on_ch10_dac_deleted_text()
+{
+	channel_text_deleted(10);
+}
+
+void on_ch10_dac_inserted_text()
+{
+	channel_text_inserted(10);
+}
+
+void on_ch11_dac_deleted_text()
+{
+	channel_text_deleted(11);
+}
+
+void on_ch11_dac_inserted_text()
+{
+	channel_text_inserted(11);
+}
+
+void on_ch12_dac_deleted_text()
+{
+	channel_text_deleted(12);
+}
+
+void on_ch12_dac_inserted_text()
+{
+	channel_text_inserted(12);
+}
+
+void on_ch13_dac_deleted_text()
+{
+	channel_text_deleted(13);
+}
+
+void on_ch13_dac_inserted_text()
+{
+	channel_text_inserted(13);
+}
+
+void on_ch14_dac_deleted_text()
+{
+	channel_text_deleted(14);
+}
+
+void on_ch14_dac_inserted_text()
+{
+	channel_text_inserted(14);
+}
+
+void on_ch15_dac_deleted_text()
+{
+	channel_text_deleted(15);
+}
+
+void on_ch15_dac_inserted_text()
+{
+	channel_text_inserted(15);
+}
+
+void on_Configure_Button_clicked()
+{
+	printf("Configure button clicked\n");
+}
+
+// called when window is closed
+void on_window_main_destroy()
+{
+    gtk_main_quit();
+}
+
+
+int main(int argc, char *argv[])
+{
+    GtkBuilder      *builder; 
+    GtkWidget       *window;
+ 
+    gtk_init(&argc, &argv);
+ 
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "CFD.glade", NULL);
+ 
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    
+	GEN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "GEN_CB"));
+	GMode_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "GMode_CB"));
+	Neg_Pol_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Neg_Pol_CB"));
+	Internal_AGND_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Internal_AGND_CB")); 
+
+	Nowlin_Mode_Menu_h = GTK_WIDGET(gtk_builder_get_object(builder, "Nowlin_Mode_Menu"));
+	Nowlin_Delay_Menu_h = GTK_WIDGET(gtk_builder_get_object(builder, "Nowlin_Delay_Menu"));
+	Test_Point_Menu_h = GTK_WIDGET(gtk_builder_get_object(builder, "Test_Point_Menu"));
+	Lockout_Mode_Menu_h = GTK_WIDGET(gtk_builder_get_object(builder, "Lockout_Mode_Menu"));
+	Lockout_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Lockout_DAC_Box"));
+	AGND_Trim_Menu_h = GTK_WIDGET(gtk_builder_get_object(builder, "AGND_Trim_Menu"));
+
+
+	Channel0_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel0_EN_CB"));
+	Channel0_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel0_LE_DAC_Box"));
+
+	Channel1_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel1_EN_CB"));
+	Channel1_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel1_LE_DAC_Box"));
+
+	Channel2_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel2_EN_CB"));
+	Channel2_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel2_LE_DAC_Box"));
+
+	Channel3_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel3_EN_CB"));
+	Channel3_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel3_LE_DAC_Box"));
+
+	Channel4_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel4_EN_CB"));
+	Channel4_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel4_LE_DAC_Box"));
+
+	Channel5_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel5_EN_CB"));
+	Channel5_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel5_LE_DAC_Box"));
+
+	Channel6_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel6_EN_CB"));
+	Channel6_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel6_LE_DAC_Box"));
+
+	Channel7_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel7_EN_CB"));
+	Channel7_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel7_LE_DAC_Box"));
+
+	Channel8_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel8_EN_CB"));
+	Channel8_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel8_LE_DAC_Box"));
+
+	Channel9_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel9_EN_CB"));
+	Channel9_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel9_LE_DAC_Box"));
+
+	Channel10_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel10_EN_CB"));
+	Channel10_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel10_LE_DAC_Box"));
+
+	Channel11_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel11_EN_CB"));
+	Channel11_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel11_LE_DAC_Box"));
+
+	Channel12_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel12_EN_CB"));
+	Channel12_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel12_LE_DAC_Box"));
+
+	Channel13_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel13_EN_CB"));
+	Channel13_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel13_LE_DAC_Box"));
+
+	Channel14_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel14_EN_CB"));
+	Channel14_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel14_LE_DAC_Box"));
+
+	Channel15_EN_CB_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel15_EN_CB"));
+	Channel15_LE_DAC_Box_h = GTK_WIDGET(gtk_builder_get_object(builder, "Channel15_LE_DAC_Box"));
+
+
+    gtk_builder_connect_signals(builder, NULL);
+    printf("connect returned\n");
+    
+    g_object_unref(builder);
+    printf("unref returned\n");
+ 
+    gtk_widget_show(window);                
+    printf("show returned\n");
+    gtk_main();
+ 
+    return 0;
+}
+
